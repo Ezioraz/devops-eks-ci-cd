6ddeaaf715788ba14662f591ef1ca118df182931
Javid Akthar <ezioraz@gmail.com>
2025-12-04 19:02:21 +0530

    terra update

--- infra/terraform/eks.tf
+++ infra/terraform/eks.tf
@@ -1,6 +1,6 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 20.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
   cluster_name    = "${var.project_name}-eks"
   cluster_version = "1.32"
@@ -8,11 +8,14 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
+
   eks_managed_node_groups = {
     default = {
-      desired_size  = 2
-      max_size      = 3
-      min_size      = 1
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
       instance_types = ["t3.small"]
       capacity_type  = "ON_DEMAND"
     }
