6e18f02f7b7bb454bfa8185e390c2937fa018c80
Javid Akthar <ezioraz@gmail.com>
2025-12-04 20:38:45 +0530

    Kubeupdate

--- infra/terraform/eks.tf
+++ infra/terraform/eks.tf
@@ -8,9 +8,58 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  # Allow laptop + Jenkins to reach EKS API server
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = false
 
+  #######################################################################
+  # EKS Access Entries (2024–2025 RBAC Model)
+  # These give IAM users full kubectl cluster-admin access
+  #######################################################################
+  access_entries = {
+    
+    cicd_user_access = {
+      principal_arn = "arn:aws:iam::447407244516:user/cicd-user"
+      type          = "STANDARD"
+
+      # Must be empty (AWS forbids system:* groups)
+      kubernetes_groups = []
+
+      # Full cluster-admin privileges
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+
+    jenkins_ci_access = {
+      principal_arn = "arn:aws:iam::447407244516:user/jenkins-ci"
+      type          = "STANDARD"
+
+      # Must be empty (no system: groups allowed)
+      kubernetes_groups = []
+
+      # Full cluster-admin privileges for Jenkins user
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+
+  #######################################################################
+  # Node Group
+  #######################################################################
   eks_managed_node_groups = {
     default = {
       desired_size   = 2
@@ -21,6 +70,9 @@ module "eks" {
     }
   }
 
+  #######################################################################
+  # Tags
+  #######################################################################
   tags = {
     Project = var.project_name
   }
